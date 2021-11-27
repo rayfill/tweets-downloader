e52d7372020818d9cc120c149eba2dbbbd35ace1
rayfill <rayfill@gmail.com>
2021-11-27 13:36:08 +0900

    fix version

--- dist/main.user.js
+++ dist/main.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         save tweet with media contents
 // @namespace    http://twitter.com/
-// @version      20210827.2
+// @version      20211127.0
 // @description  save tweet with media contents
 // @downloadURL  https://raw.githubusercontent.com/rayfill/tweets-downloader/main/dist/main.user.js
 // @updateURL    https://raw.githubusercontent.com/rayfill/tweets-downloader/main/dist/main.user.js
